7a0beba247bac0151a9d03db2e77e810adf0e158
Pierre Busnel <pierre@MacPetePro.local>
2018-04-06 18:39:02 +0200

    initial commit

---
+++ src/main/java/com/pierre2803/yogacal/YogacalApplication.java
@@ -0,0 +1,12 @@
+package com.pierre2803.yogacal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogacalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogacalApplication.class, args);
+	}
+}
